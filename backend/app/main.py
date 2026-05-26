5ca2724ea22fe158cc39cbcec10804a4c59ebcde
Vamshi <vamshivamsharaj123@gmail.com>
2026-05-26 16:39:53 +0530

    fix asyncpg

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://ai-notes-workspace-pi.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
